3abfc9c9bbeff6e69940f683c97477de889e7e2c
IshratJahanUsha <ishratjahanusha2021@gmail.com>
2022-06-03 01:02:13 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        brightOrange: '#F54B3B',
+        deepOrange: '#F54B3C',
+        darkBlack: '#000000',
+        lightYellow: '#F5EDDF',
+      },
+    },
+  },
+  plugins: [],
+}
